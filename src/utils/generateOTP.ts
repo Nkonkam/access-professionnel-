c8a621744b833b6606b5b871a5ae5095f435bed1
daulin-wandji <daulwangithub@gmail.com>
2024-03-25 15:00:20 +0100

    modifier le code otp pour produire un nombre a 6 chiffre

--- src/utils/generateOTP.ts
+++ src/utils/generateOTP.ts
@@ -1,6 +1,7 @@
 export const generateOTP = async () => {
   try {
-    return `${Math.floor(1000 + Math.random() * 9000)}`;
+    // Générer un nombre aléatoire entre 100000 (inclus) et 999999 (inclus)
+    return `${Math.floor(100000 + Math.random() * 900000)}`;
   } catch (error) {
     throw error;
   }
