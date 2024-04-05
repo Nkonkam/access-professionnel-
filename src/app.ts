e4db802af3093b7e199c10804f7ad118256a1f36
daulin-wandji <daulwangithub@gmail.com>
2024-04-05 06:19:52 +0100

    ajouter un middleware de verification d erreur errorHandler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import cors from "cors";
 import morgan from "morgan";
 import express from "express";
 
+import { errorHandler } from "./middlewares/errorHandler";
+
+import { verifyTokenAccessAPI } from "./middlewares/verifyTokenAccessAPI";
+
 import router from "./routes";
 
 const app = express();
@@ -13,6 +17,8 @@ app.use(
   })
 );
 
+// app.use(verifyTokenAccessAPI);
+
 app.use(morgan("combined"));
 
 app.use(express.json());
@@ -20,5 +26,7 @@ app.use(express.json());
 app.use("/api/v1/learner/profile_picture", express.static("public/uploads"));
 
 app.use("/api/v1", router);
+
+app.use(errorHandler);
 
 export default app;
