a9b566cdb59e156771d339f2e22ae1f82d80d9b3
daulin-wandji <daulwangithub@gmail.com>
2024-04-05 06:37:25 +0100

    modification du model  Learner

--- src/domaines/learners/model.ts
+++ src/domaines/learners/model.ts
@@ -4,31 +4,27 @@ import mongoose from "mongoose";
 export interface ILearner {
   verified?: boolean;
   password: string;
-  personalInfo: {
-    image?: string;
-    fullName: string;
-    email: string;
-    phoneNumber: string;
-    dateOfBirth: Date;
-    neighborhood?: string;
-    city?: string;
-    department?: string;
-    originalDistrict?: string;
-  };
-  professionalInfo: {
-    fslc?: boolean;
-    gceOL?: boolean;
-    gceAL?: boolean;
-    phd?: boolean;
-    cep?: boolean;
-    bepc?: boolean;
-    probatoire?: boolean;
-    baccalaureat?: boolean;
-    bts?: boolean;
-    licence?: boolean;
-    master?: boolean;
-    doctorat?: boolean;
-  };
+  image?: string;
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  dateOfBirth: Date;
+  neighborhood?: string;
+  city?: string;
+  department?: string;
+  originalDistrict?: string;
+  fslc?: boolean;
+  gceOL?: boolean;
+  gceAL?: boolean;
+  phd?: boolean;
+  cep?: boolean;
+  bepc?: boolean;
+  probatoire?: boolean;
+  baccalaureat?: boolean;
+  bts?: boolean;
+  licence?: boolean;
+  master?: boolean;
+  doctorat?: boolean;
 }
 
 // Création du schéma Mongoose pour le document Learner
@@ -36,19 +32,16 @@ const learnerSchema = new mongoose.Schema<ILearner>(
   {
     verified: { type: Boolean, default: false },
     password: { type: String, required: true },
-    personalInfo: {
-      image: { type: String, required: false },
-      fullName: { type: String, required: true },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-          validator: (email: string) =>
-            /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
-          message: (props: { value: string }) =>
-            `${props.value} n'est pas une adresse e-mail valide!`,
-        },
+    image: { type: String, required: false },
+    fullName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email),
+        message: (props: { value: string }) =>
+          `${props.value} n'est pas une adresse e-mail valide!`,
       },
       phoneNumber: { type: String, required: true },
       dateOfBirth: { type: Date },
@@ -57,20 +50,18 @@ const learnerSchema = new mongoose.Schema<ILearner>(
       department: { type: String },
       originalDistrict: { type: String },
     },
-    professionalInfo: {
-      fslc: { type: Boolean, default: false },
-      gceOL: { type: Boolean, default: false },
-      gceAL: { type: Boolean, default: false },
-      phd: { type: Boolean, default: false },
-      cep: { type: Boolean, default: false },
-      bepc: { type: Boolean, default: false },
-      probatoire: { type: Boolean, default: false },
-      baccalaureat: { type: Boolean, default: false },
-      bts: { type: Boolean, default: false },
-      licence: { type: Boolean, default: false },
-      master: { type: Boolean, default: false },
-      doctorat: { type: Boolean, default: false },
-    },
+    fslc: { type: Boolean, default: false },
+    gceOL: { type: Boolean, default: false },
+    gceAL: { type: Boolean, default: false },
+    phd: { type: Boolean, default: false },
+    cep: { type: Boolean, default: false },
+    bepc: { type: Boolean, default: false },
+    probatoire: { type: Boolean, default: false },
+    baccalaureat: { type: Boolean, default: false },
+    bts: { type: Boolean, default: false },
+    licence: { type: Boolean, default: false },
+    master: { type: Boolean, default: false },
+    doctorat: { type: Boolean, default: false },
   },
   { timestamps: true }
 ); // Ajoute les champs createdAt et updatedAt automatiquement
